3bffb47919847addb42de3acf023797215461285
Omkar Abhishek Jha <71801300+omkarjha@users.noreply.github.com>
2023-05-03 13:44:23 +0530

    Update average_fun.c

--- Code_Practise/average_fun.c
+++ Code_Practise/average_fun.c
@@ -1,40 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int average(int);
+int average(int,int*);
 
 int main()
 {
      int n;
-     int avg, i;
+     int  i;
      printf("Enter the total number : ");
      scanf("%d", &n);
 
      int *arr = (int *)malloc(n * sizeof(int));
 
      printf("Enter the elements whose average needs to be calculated :\n");
-     for (i = 1; i <= n; i++)
+     for (i = 0; i < n; i++)
      {
           scanf("%d", &arr[i]);
      }
      printf("The elements are:\n");
-     for (i = 1; i <= n; i++)
+     for (i = 0; i < n; i++)
      {
           printf("%d \t", arr[i]);
      }
      printf("\n");
-     average(n);
+     average(n,arr);
 }
 
-int average(int n)
+int average(int n , int arr[])
 {
      int avg, i, sum = 0;
-     int *arr = (int *)malloc(n * sizeof(int));
-     for (i = 0; i <= n; i++)
+     for (i = 0; i < n; i++)
      {
           /* code */
-          sum += arr[i];
+          sum+= arr[i];
      }
      avg = sum / n;
      printf("The average of is : %d", avg);
+     return 0;
 }
