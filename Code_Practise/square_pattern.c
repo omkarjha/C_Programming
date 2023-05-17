fe8d749e8897ce5b453a51beaa7cb27d6372de3f
Omkar Abhishek Jha <jhaomkar396@gmail.com>
2023-05-18 01:50:03 +0530

    pattern code

---
+++ Code_Practise/square_pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>  
+ int main()  
+{  
+    int rows,i,j;  
+    printf("Enter the number of rows :");  
+    scanf("%d",&rows);  
+    for(i=0;i<rows;i++)  
+    {  
+        for(j=0;j<rows;j++)  
+        {  
+            printf("*");  
+        }  
+        printf("\n");  
+    }  
+      
+    return 0;  
+}  
